3cccdaaaa3447152b6f7058f3108447b0c358266
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-08-21 17:40:36 -0300

    requisito 10 concluido

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Missions from './data/missions';
 
 class App extends React.Component {
   render() {
